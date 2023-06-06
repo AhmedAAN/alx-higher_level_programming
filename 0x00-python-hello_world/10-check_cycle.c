a9b75b9a361378081693a3427b00fee658001a64
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-06-05 18:18:56 -0700

    hot fix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,25 +1,28 @@
 #include "lists.h"
 
 /**
- * checks if a singly linked list has a cycle in it
+ * check_cycle - checks if a singly linked list has a cycle in it
  * @list: a pointer to the begining of the list
  * Return: 0 if there is no loop or 1 if there is loop
  *
  */
 int check_cycle(listint_t *list)
 {
-        listint_t *current;
-        int check;
+	listint_t *current;
+	int check;
 
-        current = list;
-        check = curretn->n;
+	if (!list)
+		return (0);
+	current = list;
+	check = current->n;
 
-        current = current->next;
-        while (current != NULL)
-        {
-                if (current->n == check)
-                        return (1);
-                current = current->next;
-        }
-        return (0);
+	current = current->next;
+	while (current != NULL)
+	{
+		if (current->n == check)
+			return (1);
+		current = current->next;
+	}
+
+	return (0);
 }
