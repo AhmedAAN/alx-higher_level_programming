ab4be604e1dd2b1e8748ac9e505091bd9a50b87e
AhmedAAN <109803368+AhmedAAN@users.noreply.github.com>
2023-06-07 02:33:42 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 99):
-    print("{0:02d}".format(i), end = "")
-    if i != 1:
-        print(end=", ")
+    print("{0:02d}".format(i), end = ", ")
 print("99")
