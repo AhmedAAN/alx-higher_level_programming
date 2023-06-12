ece1e9bde8ab4e769f2ed01dd34d0bfceffb0a2e
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-06-12 14:01:53 -0700

    new tasks

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     new = my_list.copy()
     if idx < 0 and idx > len(my_list) - 1:
-        return (new)
+        return (my_lis.copy()t)
     new[idx] = element
     return (new)
