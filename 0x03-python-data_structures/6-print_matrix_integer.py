057dbef3fe4eab36b87c1cdc923127ad56733f81
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-06-13 00:14:25 -0700

    6-print_matrix_intger.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in range(0, len(matrix)):
+        for point in range(0, len(matrix[row])):
+            if point != 0:
+                print(end=" ")
+            print("{}".format(matric[row][point]), end="")
+        print("")
