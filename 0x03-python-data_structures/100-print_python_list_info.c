f890fc1f1d6690a29e6172c6a9e003581ace38d9
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-06-13 06:33:53 -0700

    advanced task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,16 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size = PyList_Size(p);
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i <= size; i++)
+	{
+		printf("Element %i: %s\n", i, obj->ob_item[i]->ob_type->tp_name)
+	}
+}
