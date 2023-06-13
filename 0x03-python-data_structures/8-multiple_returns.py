642bfc50a1a7ac4dab5d8b1959c54857fd7a79be
AhmedAAN <109803368+AhmedAAN@users.noreply.github.com>
2023-06-13 10:57:13 +0300

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         new = (0, None)
     else:
-        new = (len(sentence, senctence{0}))
+        new = (len(sentence), senctence{0})
     return new
