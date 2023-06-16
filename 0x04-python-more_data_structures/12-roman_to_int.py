269a4936c3ddf9d6cdfac8006b565f55de9583db
AhmedAAN <109803368+AhmedAAN@users.noreply.github.com>
2023-06-17 00:12:17 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,9 +7,9 @@ def roman_to_int(roman_string):
     v = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     total = 0
     for ch in range(len(roman_string)):
-        current_ch = roman_string[ch]
-        if ch < len(roman_string) - 1 and v[roman_string[ch + 1]] > v[current_ch]:
-            total -= v[current_ch]
+        cur_ch = roman_string[ch]
+        if ch < len(roman_string) - 1 and v[roman_string[ch + 1]] > v[cur_ch]:
+            total -= v[cur_ch]
         else:
-            total += v[current_ch]
+            total += v[cur_ch]
     return (total)
