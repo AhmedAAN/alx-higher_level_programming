38bb4ebe783870f49029eeb35e323d9dc4b52b9c
AhmedAAN <109803368+AhmedAAN@users.noreply.github.com>
2023-08-16 12:46:04 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
-        return (None)
+        return None
     values = list(a_dictionary.values())
-    index = values.index(max(values))
     keys = list(a_dictionary.keys())
-    return (keys[index])
+    maxValueIndex = values.index(max(values))
+    return keys[maxValueIndex]
