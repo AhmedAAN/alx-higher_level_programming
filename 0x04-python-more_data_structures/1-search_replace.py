d284c284992888ab4131b7d3f53ae551e7b02000
AhmedAAN <109803368+AhmedAAN@users.noreply.github.com>
2023-08-16 13:54:45 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_matrix = list(map(lambda x: replace if x == search else x, my_list))
-    return (new_matrix)
+    return list(map(lambda x: replace if x == search else x, my_list))
