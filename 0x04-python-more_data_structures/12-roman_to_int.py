a1015c304e36c8ed3094014c287800d8a6fd0e05
AhmedAAN <109803368+AhmedAAN@users.noreply.github.com>
2023-08-16 13:29:18 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
-    if not isinstance(roman_string, str):
-        return 0
-    v = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+    romanToNumber = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, "M": 1000}
     total = 0
-    for ch in range(len(roman_string)):
-        cur_ch = roman_string[ch]
-        if ch < len(roman_string) - 1 and v[roman_string[ch + 1]] > v[cur_ch]:
-            total -= v[cur_ch]
+    for position in range(len(roman_string)):
+        currentLetter = roman_string[position]
+        currentValue = romanToNumber[currentLetter]
+        if position < len(roman_string) - 1 and currentValue < romanToNumber[roman_string[position + 1]]:
+            total -= currentValue
         else:
-            total += v[cur_ch]
-    return (total)
+            total += currentValue
+    return total
