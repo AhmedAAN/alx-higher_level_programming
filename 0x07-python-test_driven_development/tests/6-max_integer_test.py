a8ea398909423047fd30dbb7784355b335be337d
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-09-03 15:46:58 +0300

    unittest tasks completed

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -18,5 +18,39 @@ class TestMaxInteger(unittest.TestCase):
         unorderd = [1, 5, 3, -6]
         self.assertEqual(max_integer(unorderd), 5)
 
+    def testMaxAtTheBeginning(self):
+        '''Test a list with the max value at the beginning'''
+        beginning = [4, 1, 2, 3]
+        self.assertEqual(max_integer(beginning), 4) 
+
+    def testEmptyList(self):
+        '''Test an empty list'''
+        empty = []
+        self.assertEqual(max_integer(empty), None)
+
+    def testOneElement(self):
+        '''Test a one element list'''
+        oneElement = [7]
+        self.assertEqual(max_integer(oneElement), 7)
+
+    def testFloats(self):
+        '''Test a float list'''
+        floats = [2.5, 1.7, -1.8, 8.9]
+        self.assertEqual(max_integer(floats), 8.9)
+
+    def testFloatsAndIntegers(self):
+        '''Test a floats and integers list'''
+        floatsAndIntegers = [1, 2, -7, 9.2]
+        self.assertEqual(max_integer(floatsAndIntegers), 9.2)
+
+    def testString(self):
+        '''Test a string'''
+        string = "Hello"
+        self.assertEqual(max_integer(string), 'o')
+
+    def testListOfStrings(self):
+        '''Test list of strings'''
+        listString = ["Hello", "World", "Good", "People"]
+        self.assertEqual(max_integer(listString), "World")
 if __name__ == '__main__':
     unittest.main()
