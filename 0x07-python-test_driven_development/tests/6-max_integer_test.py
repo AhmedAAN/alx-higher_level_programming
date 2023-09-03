a445feeb5ff21a65ec576989d6c1440c7f7c5ae8
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-09-03 15:27:42 +0300

    unittest tasks completed

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,5 +13,10 @@ class TestMaxInteger(unittest.TestCase):
         ordered = [1, 2, 3, 4]
         self.assertEqual(max_integer(ordered), 4)
 
+    def testUnorderdList(self):
+        '''Test an unordered list of integers'''
+        unorderd = [1, 5, 3, -6]
+        self.assertEqual(max_integer(unorderd), 5)
+
 if __name__ == '__main__':
     unittest.main()
