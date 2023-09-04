359f66eaf74362f492678857756f3af2756d7bf2
AhmedAAN <109803368+AhmedAAN@users.noreply.github.com>
2023-09-04 23:45:52 +0300

    1-rectangle created

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""Define a class Rectangle ."""
+
+
+class Rectangle:
+    """Represent a Rectangle ."""
+
+    def __init__(self, width=0, height=0):
+        """Intialize a new rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The length of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/sets width of the rectangle."""
+        return this.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        
+        this.__width = value
+
+    @property
+    def height(self):
+        """Get/sets height of the rectangle."""
+        return this.__height
+
+    @width.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        
+        this.__width = value
