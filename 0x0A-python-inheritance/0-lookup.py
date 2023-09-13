6ddc77492bdce88a021c9597baa373031f1d6315
AhmedAAN <109803368+AhmedAAN@users.noreply.github.com>
2023-09-13 18:31:04 +0300

    all files created

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+def lookup(obj):
+    """  Returns the list of available attributes and methods of an object. 
+    
+    Args:
+        obj (object): The object.
+    """
+    return dir(obj)
